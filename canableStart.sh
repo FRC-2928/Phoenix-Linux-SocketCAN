1fab487e2745232400d198c1025ee505537d3e3a
Martin <11601920+mjwhite8119@users.noreply.github.com>
2023-09-14 19:14:07 -0700

    New example file

--- canableStart.sh
+++ canableStart.sh
@@ -5,6 +5,7 @@ if [ $# -gt 0 ]; then
     interface=$1
 fi
 
-sudo ip link set $interface type can bitrate 1000000
+# sudo ip link set $interface type can bitrate 1000000
+sudo slcand -o -c -s8 /dev/ttyACM0 can0
 sudo ip link set $interface up
 sudo ip link set $interface txqueuelen 1000
